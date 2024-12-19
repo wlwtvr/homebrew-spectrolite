d079637d0bdeda95b10a8f0baab3bc9f452e2728
A Dzulfikar SR <ahmaddzulfikarsr@gmail.com>
2024-12-19 11:03:42 +0700

    init

---
+++ Casks/spectrolite.rb
@@ -0,0 +1,14 @@
+cask "spectrolite" do
+  arch arm: "-silicon", intel: ""
+
+  version :latest
+  sha256 :no_check
+
+  url "https://spectrolite.app/downloads/Spectrolite#{arch}.dmg"
+  name "Spectrolite"
+  desc "a free Mac desktop app by ANEMONE to make colorful 🌈 risograph prints and zines more easily."
+  homepage "https://spectrolite.app/"
+
+  app "Spectrolite.app"
+
+end
